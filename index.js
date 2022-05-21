2be3866a14aa4c778d436cb07f94916923586043
Krix12 <kristjan.kocevar89@gmail.com>
2022-05-21 14:41:54 +0200

    second

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 var elements = document.getElementsByClassName("el");
 let currentSlide = 0;
-async function slide() {
-  console.log("click")
- elements(currentSlide).style.animation = "outSlide 0.5s";
-  await sleep(450);
-  elements[currentSlide].style.display = "none"
-  currentslide++
-  elements[currentSlide].style.visibility = "visible";
-  elements[currentSlide].style.animation = "inSlide 0.5s";
+var leftArrow = document.getElementById("arrow-left");
+var rightArrow = document.getElementById("arrow-right");
+leftArrow.style.display = "none";
+async function slideForwards() {
+
+  elements[currentSlide].style.animation = "outSlide 0.5s";
+  await sleep(200)
+  elements[currentSlide].style.display = "none";
+  if(currentSlide  === elements.length - 2) {
+    rightArrow.style.display = "none";
+  }
+  currentSlide++;
+
+  leftArrow.style.display = "inherit";
+  elements[currentSlide].style.display = "inherit";
+  elements[currentSlide].style.animation = "inSlide 0.5s reverse";
+ 
+
+
 }
 
+async function slideBackwards() {
+rightArrow.style.display = "inherit";
+elements[currentSlide].style.display = "none";
+currentSlide--;
+if(currentSlide === 0) document.getElementById("arrow-left").style.display = "none";
+elements[currentSlide].style.display = "inherit";
+elements[currentSlide].style.animation = "outSlide 0.5s reverse";
+ 
+}
